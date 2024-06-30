cd66c983965c7b671a00a6b058a789fb9752a375
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-06-30 15:40:55 +0000

    seed追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+end
+
+User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+end
+# Users = User.create!(
+#   [
+#     {email: 'olivia@example.com', name: 'Olivia', password: 'password', password_confirmation: 'password'},
+#     {email: 'james@example.com', name: 'James', password: 'password', password_confirmation: 'password'},
+#     {email: 'lucas@example.com', name: 'Lucas', password: 'password', password_confirmation: 'password'}
+#   ]
+# )
+
+User.all.each do |user|
+  user.posts.create!(
+    title: 'Sample title',
+    body: 'Sample text'
+  )
+end
+
+Genre.create!(
+  [
+    {name: 'Jeans'},
+    {name: 'Blouse'},
+    {name: 'T-shirt'},
+    {name: 'Skirt'},
+    {name: 'Sweater'},
+    {name: 'Kimono'},
+    {name: 'Cardigan'},
+    {name: 'Knitwear'},
+    {name: 'Babysuits'},
+  ]
+  )
+  
