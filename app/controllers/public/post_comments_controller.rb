d0db35bc0fa1706aae584e7103de4cbc8017b3e8
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-07-22 14:33:44 +0000

    バリデーション修正

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,8 +4,14 @@ class Public:: PostCommentsController < ApplicationController
     post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+  
+    if comment.save
+      flash[:notice] = 'Your Review is Successfully Shared.'
+      redirect_to post_path(post)
+    else
+      flash.now[:alert] = 'Failed to share the comment.'
+      render :show
+    end
   end
 
   private
@@ -13,5 +19,4 @@ class Public:: PostCommentsController < ApplicationController
   def post_comment_params
     params.require(:post_comment).permit(:comment, :star)
   end
-  
 end
